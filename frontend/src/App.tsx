2b8fc9caf5c1f7f6bf98b9813030b185525b55f0
Miguel Laginha <me@miguellaginha.com>
2022-09-17 13:54:34 +0100

    chore(setup): setup xo, prettier, tailwind and postcss all working fine

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,59 +1,74 @@
-import { useEffect } from "react";
-import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+/* eslint-disable unicorn/filename-case */
+import { useEffect } from 'react';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 
-const style = { display: "flex", gap: "8px", padding: "8px" };
+const style = { display: 'flex', gap: '8px', padding: '8px' };
 
 function App() {
-  /**
-   * During development we can still access the base path at `/`
-   * And this hook will make sure that we land on the base `/app`
-   * path which will mount our App as usual.
-   * In production, Phoenix makes sure that the `/app` route is
-   * always mounted within the first request.
-   * */
-  useEffect(() => {
-    if (window.location.pathname === "/") {
-      window.location.replace("/app");
-    }
-  }, []);
+	/**
+	 * During development we can still access the base path at `/`
+	 * And this hook will make sure that we land on the base `/app`
+	 * path which will mount our App as usual.
+	 * In production, Phoenix makes sure that the `/app` route is
+	 * always mounted within the first request.
+	 */
+	useEffect(() => {
+		if (window.location.pathname === '/') {
+			window.location.replace('/app');
+		}
+	}, []);
 
-  return (
-    <BrowserRouter basename="app">
-      <nav style={style}>
-        <Link to="/">Home</Link>
-        <Link to="/settings">Settings</Link>
-        <button class="btn">Button</button>
-        <br />
-      </nav>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="settings" element={<SettingsPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter basename="app">
+			<nav style={style}>
+				<Link to="/">Home</Link>
+				<Link to="/settings">Settings</Link>
+				<button className="btn">Button</button>
+
+				<div className="card w-96 bg-base-100 shadow-xl">
+					<figure>
+						<img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+					</figure>
+					<div className="card-body">
+						<h2 className="card-title">Shoes!</h2>
+						<p>If a dog chews shoes whose shoes does he choose?</p>
+						<div className="card-actions justify-end">
+							<button className="btn btn-primary">Buy Now</button>
+						</div>
+					</div>
+				</div>
+
+				<br />
+			</nav>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="settings" element={<SettingsPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 function SettingsPage() {
-  return (
-    <div>
-      <h1>Settings Page</h1>
-      <ul>
-        <li>My profile</li>
-        <li>Music</li>
-        <li>About</li>
-      </ul>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="text-3xl font-bold underline">Settings</h1>
+			<ul>
+				<li>My profile</li>
+				<li>Music</li>
+				<li>About</li>
+			</ul>
+		</div>
+	);
 }
 
 function HomePage() {
-  const style = { padding: "8px" };
-  return (
-    <div style={style}>
-      <h1>React TS Home</h1>
-      <p>Welcome to the homepage</p>
-    </div>
-  );
+	const style = { padding: '8px' };
+	return (
+		<div style={style}>
+			<h1 className="text-3xl font-bold underline">React TS home</h1>
+			<p>Welcome to the homepage</p>
+		</div>
+	);
 }
 
 export default App;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,31 +1,33 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  /**
-   * Using the `webapp` base path for production builds
-   * so we can leverage Phoenix static assets plug to deliver
-   * our React app directly from our final Elixir app,
-   * Serving all files from the `priv/static/webapp` folder.
-   * NOTE: Remember to move the frontend build files to the
-   * `priv` folder during the application build process in CI
-   */
-  // @ts-ignore
-  base: process.env.NODE_ENV === "production" ? "/webapp/" : "/",
-  // Forward all requests made by our React frontend to `localhost:3000/api`
-  // to our Phoenix backend running at `localhost:4000`.
-  // This is only necessary during development.
-  // In production, our Phoenix and React apps are served from the same
-  // domain and port, which makes this configuration unecessary.
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:4000",
-        secure: false,
-        ws: true,
-      },
-    },
-  },
+	plugins: [react()],
+	/**
+	 * Using the `webapp` base path for production builds
+	 * so we can leverage Phoenix static assets plug to deliver
+	 * our React app directly from our final Elixir app,
+	 * Serving all files from the `priv/static/webapp` folder.
+	 * NOTE: Remember to move the frontend build files to the
+	 * `priv` folder during the application build process in CI
+	 */
+	// @ts-expect-error
+	base: process.env.NODE_ENV === 'production' ? '/webapp/' : '/',
+	/**
+	 * Forward all requests made by our React frontend to `localhost:3000/api`
+	 * to our Phoenix backend running at `localhost:4000`.
+	 * This is only necessary during development.
+	 * In production, our Phoenix and React apps are served from the same
+	 * domain and port, which makes this configuration unecessary.
+	 */
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:4000',
+				secure: false,
+				ws: true,
+			},
+		},
+	},
 });
