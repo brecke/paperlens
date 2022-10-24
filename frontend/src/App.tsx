6280f09f7a52f25e902686be4b0b6b84ba4fd0e6
Miguel Laginha <me@miguellaginha.com>
2022-10-24 22:07:35 +0100

    refactor: moved things around a little bit; also added new links

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,61 +1,88 @@
 /* eslint-disable unicorn/filename-case */
-import { useEffect } from 'react';
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import { useEffect } from "react";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 
-import PaperForm from './dashboard/paperform';
+import SearchForm from "./search-view";
+import SkillForm from "./skill-view";
+import Dashboard from "./dashboard-view";
 
-const style = { display: 'flex', gap: '8px', padding: '8px' };
+const style = { display: "flex", gap: "8px", padding: "8px" };
 
 function App() {
-	/**
-	 * During development we can still access the base path at `/`
-	 * And this hook will make sure that we land on the base `/app`
-	 * path which will mount our App as usual.
-	 * In production, Phoenix makes sure that the `/app` route is
-	 * always mounted within the first request.
-	 */
-	useEffect(() => {
-		if (window.location.pathname === '/') {
-			window.location.replace('/app');
-		}
-	}, []);
+  /**
+   * During development we can still access the base path at `/`
+   * And this hook will make sure that we land on the base `/app`
+   * path which will mount our App as usual.
+   * In production, Phoenix makes sure that the `/app` route is
+   * always mounted within the first request.
+   */
+  useEffect(() => {
+    if (window.location.pathname === "/") {
+      window.location.replace("/app");
+    }
+  }, []);
 
-	return (
-		<BrowserRouter basename="app">
-			<nav style={style}>
-				<Link to="/">Dashboard</Link>
-				<Link to="/settings">Settings</Link>
-				<br />
-			</nav>
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="settings" element={<SettingsPage />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter basename="app">
+      <nav style={style}>
+        <Link to="/">Homepage</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/search">Search</Link>
+        <Link to="/settings">Settings</Link>
+        <br />
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<DashboardView />} />
+        <Route path="/search" element={<SearchView />} />
+				<Route path="/publication/:id" element={<PublicationView />} />
+        <Route path="settings" element={<SettingsView />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-function SettingsPage() {
-	return (
-		<div className="container mx-auto">
-			<h1 className="text-3xl font-bold underline">Settings</h1>
-			<ul>
-				<li>My profile</li>
-				<li>Music</li>
-				<li>About</li>
-			</ul>
-		</div>
-	);
+function SettingsView() {
+  return (
+    <div className="container mx-auto">
+      <h1>Settings page</h1>
+    </div>
+  );
 }
 
 function HomePage() {
-	const style = { class: 'container mx-auto', padding: '8px' };
-	return (
-		<div style={style}>
-			<h1 className="text-3xl font-bold underline">Dashboard</h1>
-			<PaperForm />
-		</div>
-	);
+  const style = { class: "container mx-auto", padding: "8px" };
+  return (
+    <div style={style}>
+      <h1>Homepage</h1>
+    </div>
+  );
+}
+
+function DashboardView() {
+  const style = { class: "container mx-auto", padding: "8px" };
+  return (
+    <div style={style}>
+      <Dashboard />
+    </div>
+  );
+}
+function SearchView() {
+  const style = { class: "container mx-auto", padding: "8px" };
+  return (
+    <div style={style}>
+			<SearchForm />
+    </div>
+  );
+}
+
+function PublicationView() {
+  const style = { class: "container mx-auto", padding: "8px" };
+  return (
+    <div style={style}>
+			 <SkillForm />
+    </div>
+  );
 }
 
 export default App;
